384f6cb94fc68adadb2c15d9a107a21ebf97ee7e
Pedronane <march3sipietro@gmail.com>
2025-02-12 19:07:24 +0100

    The install script should now work

--- installScripts/install.sh
+++ installScripts/install.sh
@@ -8,15 +8,23 @@ cd ~/
 
 # Install pkgs
 yay -S pywal swww
-wal -i ~/Dotfiles/wallpapers/Frieren-Dead-Stare.png -n
-yay -S waybar sddm swaync-client starship myfetch uwsm neovim python-pywalfox hypridle hyprpicker hyprshot hyprlock pyprland wlogout fd cava brightnessctl nerd-fonts nwg-look htop blueman bluez stow hyprland wofi cliphist grim slurp libnotify xdg-desktop-portal-hyprland hyprpolkitagent-bin qt5-wayland qt6-wayland tmux zoxyde zen-browser-bin zsh kitty yazi thunar
+wal -i ~/dotfiles/wallpapers/Frieren-Dead-Stare.png -n
+yay -S waybar sddm swaync-client starship myfetch uwsm neovim python-pywalfox hypridle hyprpicker hyprshot hyprlock pyprland wlogout fd cava brightnessctl nerd-fonts nwg-look htop blueman bluez stow hyprland wofi cliphist grim slurp libnotify xdg-desktop-portal-hyprland hyprpolkitagent-bin qt5-wayland qt6-wayland tmux zen-browser-bin zsh kitty yazi thunar
 systemctl enable bluetooth
 systemctl enable sddm.service # Don't know if it work
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
 yay -S pipewire pipewire-pulse pipewire-alsa pipewire-jack pavucontrol pulsemixer 
 systemctl --user enable pipewire.service pipewire-pulse.service
 systemctl --user start pipewire.service pipewire-pulse.service
 
+# Change shell
+chsh -s /usr/bin/zsh
+
+# Hyprland Plugin
+hyprpm update
+hyprpm add https://github.com/virtcode/hypr-dynamic-cursors
+hyprpm enable dynamic-cursors
+
 #Copy dotfiles
-git clone https://github.com/Pedronane/dotfiles
-cd dotfiles
+cd ~/dotfiles
 stow .
