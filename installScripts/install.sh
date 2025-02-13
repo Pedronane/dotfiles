91167d96c270f72705e2863478108b31e47f6a6e
Pedronane <march3sipietro@gmail.com>
2025-02-13 15:29:19 +0100

    Added some things to the script

--- installScripts/install.sh
+++ installScripts/install.sh
@@ -7,23 +7,20 @@ makepkg -si
 cd ~/
 
 # Install pkgs
-yay -S pywal swww
-wal -i ~/dotfiles/wallpapers/Frieren-Dead-Stare.png -n
-yay -S waybar sddm swaync-client starship myfetch uwsm neovim python-pywalfox hypridle hyprpicker hyprshot hyprlock pyprland wlogout fd cava brightnessctl nerd-fonts nwg-look htop blueman bluez stow hyprland wofi cliphist grim slurp libnotify xdg-desktop-portal-hyprland hyprpolkitagent-bin qt5-wayland qt6-wayland tmux zen-browser-bin zsh kitty yazi thunar
-systemctl enable bluetooth
-systemctl enable sddm.service # Don't know if it work
+yay -S waybar sddm swaync-client starship myfetch uwsm neovim python-pywalfox hypridle hyprpicker hyprshot hyprlock pyprland wlogout fd cava brightnessctl nerd-fonts nwg-look htop blueman bluez stow hyprland wofi cliphist grim slurp libnotify xdg-desktop-portal-hyprland hyprpolkitagent-bin qt5-wayland qt6-wayland tmux zen-browser-bin zsh kitty yazi thunar ripgep network-manager-applet pipewire pipewire-pulse pipewire-alsa pipewire-jack pavucontrol pulsemixer pywal swww
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
-yay -S pipewire pipewire-pulse pipewire-alsa pipewire-jack pavucontrol pulsemixer 
+
+# Generate colors
+wal -i ~/dotfiles/wallpapers/Frieren-Dead-Stare.png -n
+
+# Enable some services
+sudo systemctl enable bluetooth
+sudo systemctl enable sddm.service
 systemctl --user enable pipewire.service pipewire-pulse.service
 systemctl --user start pipewire.service pipewire-pulse.service
 
 # Change shell
 chsh -s /usr/bin/zsh
-
-# Hyprland Plugin
-hyprpm update
-hyprpm add https://github.com/virtcode/hypr-dynamic-cursors
-hyprpm enable dynamic-cursors
 
 #Copy dotfiles
 cd ~/dotfiles
