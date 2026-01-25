5af4a2308e668afb8416fc01f73a13291c2724b4
Pedronane <march3sipietro@gmail.com>
2026-01-25 19:21:17 +0100

    Polybar script and wiremix

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
-polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-# killall -q polybar
-
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybarmain.log
-polybar main 2>&1 | tee -a /tmp/polybarmain.log & disown
-
-echo "Bars launched..."
+if pgrep -x polybar > /dev/null; then
+    echo "Polybar main running, killing..." | tee -a /tmp/polybarmain.log
+    killall polybar
+else
+    echo "---" | tee -a /tmp/polybarmain.log
+    polybar main 2>&1 | tee -a /tmp/polybarmain.log & disown
+    echo "Polybar main launched..."
+fi
